b5812fc2bc1566a3044dc86ca764f9fa1e78804f
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2022-09-14 00:22:29 +0545

    Added fuzzy reflections

--- material.go
+++ material.go
@@ -22,11 +22,15 @@ func (l Lambertian) Scatter(rIn Ray, rec HitRecord) (bool, Ray, Color) {
 // Metal Material
 type Metal struct {
 	Albedo Color
+	Fuzz   float64
 }
 
 func (m Metal) Scatter(rIn Ray, rec HitRecord) (bool, Ray, Color) {
 	reflected := Reflect(UnitVector(rIn.Direction), rec.Normal)
-	scattered := Ray{rec.P, reflected}
+	scattered := Ray{
+		rec.P,
+		AddVectors(reflected, MultiplyScalar(RandomVectorInUnitSphere(), m.Fuzz)),
+	}
 	attenuation := m.Albedo
 	return (VectorDot(scattered.Direction, rec.Normal) > 0), scattered, attenuation
 }
